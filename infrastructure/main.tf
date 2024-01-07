0b3a3015bfb3298cae74cc1994868ab10693b660
NiklasMickelat <niklas.mickelat@web.de>
2024-01-08 00:42:57 +0100

    add CI Pipeline for terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,13 @@ provider "aws" {
     region = "eu-central-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-sportsvideo-to-article"
+    region = "eu-central-1"
+    key = "terraform-state"
+  }
+}
 
 variable "website_bucket_name" {
     type = string
@@ -51,12 +58,104 @@ resource "aws_s3_bucket_policy" "public_access" {
     })
 }
 
+/* Wird im Actions Workflow gepushed, weil es ein Artefakt ist und dann kann man auch nur für pushes auf das Artefakt einen deploy workflow bauen 
 resource "aws_s3_object" "index_upload" {
     bucket          = aws_s3_bucket.website_bucket.id
     key             = var.html_file_name
     source          = "../${var.html_file_name}" # Pfad zur lokalen index.html-Datei
     content_type    = "text/html"
 }
+*/
+
+
+
+
+
+
+/* GitHub Repo und Github-Actions Workflow selbst auch über Terraform anlegen lassen, falls Änderungen am Repo oder am Workflow commitet werden
+
+# User für S3 Bucket Zugriff anlegen
+
+resource "aws_iam_user" "github_actions_user" {
+  name = "github-actions-user"
+}
+
+resource "aws_iam_access_key" "github_actions_access_key" {
+  user = aws_iam_user.github_actions_user.name
+}
+
+resource "aws_iam_user_policy_attachment" "github_actions_user_policy" {
+  user       = aws_iam_user.github_actions_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+output "github_actions_user_access_key" {
+    value = "AWS S3 Github Actions User AccessKey: ${aws_iam_access_key.github_actions_access_key.secret}"
+}
+
+
+# Github Repo und Actions-Workflow anlegen
+
+provider "github" {
+  token = "YOUR_GITHUB_TOKEN" # Sicherheitstechnisch schlecht, weil im eigenen Repo der Token für den ganzen Github-Account liegt. Aber das Repo kann nicht ausversehen public werden, da das eben IaC sicherstellt.
+}
+
+resource "github_repository" "terraform_repo" {
+  name        = "SportsVideoToArticle"
+  private     = true
+}
+
+resource "github_actions_workflow" "terraform_workflow" {
+  name     = "terraform-workflow"
+  repository = github_repository.terraform_repo.name
+
+  on = "push"
+
+  # Hier kannst du die Konfiguration für deine GitHub Actions definieren
+  content = <<-YAML
+    name: Terraform Workflow
+
+    on:
+      push:
+        branches:
+          - main
+
+    jobs:
+      terraform:
+        runs-on: ubuntu-latest
+
+        steps:
+        - name: Checkout repository
+          uses: actions/checkout@v2
+
+        - name: Set up Terraform
+          uses: hashicorp/setup-terraform@v1
+          with:
+            terraform_version: latest
+
+        - name: Terraform Init
+          run: terraform init -backend-config="bucket=your-s3-bucket-name" -backend-config="key=terraform-state"
+
+        - name: Terraform Apply
+          run: terraform apply -auto-approve
+
+        - name: Terraform Apply
+          run: terraform apply -auto-approve
+  YAML
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+/* Outputs */
 
 output "website_url" {
     value = "https://${aws_s3_bucket.website_bucket.bucket_regional_domain_name}/${var.html_file_name}"
