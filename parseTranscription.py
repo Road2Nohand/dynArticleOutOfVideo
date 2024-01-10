af2719634ec69a0c7a9ad8304ace7b23bee5a793
NiklasMickelat <niklas.mickelat@web.de>
2024-01-10 16:30:22 +0100

    add function to clean stadion noise

--- parseTranscription.py
+++ parseTranscription.py
@@ -6,6 +6,8 @@ file_path = 'BundesligaSpiel1std37min.json'  # Ersetzen Sie dies mit dem tatsäc
 
 # Output-Datei, ist der Name der Input Datei mit '_transcript_parsed.json' am Ende
 out_file_path = file_path.replace('.json', '_transcript_parsed.json')
+# Output-Datei für die bereinigte Version
+cleaned_out_file_path = file_path.replace('.json', '_transcript_cleaned_from_noise.json')
 
 # JSON-Datei laden
 with open(file_path, 'r') as file:
@@ -13,6 +15,7 @@ with open(file_path, 'r') as file:
 
 # Liste für gespeicherte Transkripte erstellen
 transcripts = []
+cleaned_transcripts = []  # Für die bereinigte Version
 
 # Durch alle Transkripte gehen
 for entry in data.get('Transcript', []):
@@ -41,6 +44,11 @@ for entry in data.get('Transcript', []):
             "content": content
         }
         transcripts.append(transcript_entry)
+        
+        # Überprüfen, ob sowohl Loudness- als auch Confidence-Durchschnitt größer oder gleich 0.2 sind
+        if loudness_average is not None and confidence_average is not None and \
+           loudness_average >= 0.2 and confidence_average >= 0.2:
+            cleaned_transcripts.append(transcript_entry)
 
 # Ergebnis als JSON-Datei speichern
 with open(out_file_path, 'w') as output_file:
@@ -49,3 +57,7 @@ with open(out_file_path, 'w') as output_file:
 # Ergebnis ausgeben
 for transcript in transcripts:
     print(transcript)
+
+# Bereinigte Version als JSON-Datei speichern
+with open(cleaned_out_file_path, 'w') as cleaned_output_file:
+    json.dump(cleaned_transcripts, cleaned_output_file, indent=2)
